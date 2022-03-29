02c99c290bbb435083e1230aeb88637cab464304
freddydc <freddydiaz.liv@gmail.com>
2022-03-29 11:27:58 -0500

    solve: if2

--- if/if2.rs
+++ if/if2.rs
@@ -4,13 +4,14 @@
 // Step 2: Get the bar_for_fuzz and default_to_baz tests passing!
 // Execute the command `rustlings hint if2` if you want a hint :)
 
-// I AM NOT DONE
-
 pub fn fizz_if_foo(fizzish: &str) -> &str {
+    // It's important that each conditional block return the same type!
     if fizzish == "fizz" {
         "foo"
+    } else if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
