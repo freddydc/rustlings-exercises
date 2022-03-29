516885da5506cfb5e883541ebbd84d5e395e0ed1
freddydc <freddydiaz.liv@gmail.com>
2022-03-29 11:19:02 -0500

    solve: functions5

--- functions/functions5.rs
+++ functions/functions5.rs
@@ -1,13 +1,13 @@
 // functions5.rs
 // Make me compile! Execute `rustlings hint functions5` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let answer = square(3);
     println!("The answer is {}", answer);
 }
 
 fn square(num: i32) -> i32 {
-    num * num;
+    // 1. Add a `return` ahead of `num * num;`
+    // 2. remove `;`, make it to be `num * num`
+    num * num
 }
