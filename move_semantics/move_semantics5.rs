7ed12006c5073e28d49f58f56b85cb7bc46a389a
freddydc <freddydiaz.liv@gmail.com>
2022-03-30 12:55:41 -0500

    solve: move_semantics5

--- move_semantics/move_semantics5.rs
+++ move_semantics/move_semantics5.rs
@@ -3,13 +3,11 @@
 // adding, changing or removing any of them.
 // Execute `rustlings hint move_semantics5` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
     let mut x = 100;
     let y = &mut x;
+    *y += 100; // 100 + 100
     let z = &mut x;
-    *y += 100;
-    *z += 1000;
-    assert_eq!(x, 1200);
+    *z += 1000; // 200 + 1000
+    assert_eq!(x, 1200); // x = 1200
 }
