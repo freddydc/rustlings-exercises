2b9d368b67380178f23b639d5b286ed606fda697
freddydc <freddydiaz.liv@gmail.com>
2022-04-08 11:03:09 -0500

    solve: clippy1

--- clippy/clippy1.rs
+++ clippy/clippy1.rs
@@ -6,12 +6,16 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
-// I AM NOT DONE
-
 fn main() {
-    let x = 1.2331f64;
-    let y = 1.2332f64;
-    if y != x {
+    let x = 1.2331_f64;
+    let y = 1.2332_f64;
+    let error_margin = f64::EPSILON;
+    // y != x
+    if (y - x).abs() > error_margin {
         println!("Success!");
+    }
+    // x == y
+    if (x - 1.2331_f64).abs() < error_margin {
+        println!("Pass!");
     }
 }
