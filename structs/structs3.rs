3d0d93688195b430b3bda48d32860efeea79c41d
freddydc <freddydiaz.liv@gmail.com>
2022-04-19 11:43:21 -0500

    fix(structs3): Remove redundant 'return'

--- structs/structs3.rs
+++ structs/structs3.rs
@@ -16,11 +16,11 @@ impl Package {
         if weight_in_grams <= 0 {
             panic!("Fail creating weightless package")
         } else {
-            return Package {
+            Package {
                 sender_country,
                 recipient_country,
                 weight_in_grams,
-            };
+            }
         }
     }
 
