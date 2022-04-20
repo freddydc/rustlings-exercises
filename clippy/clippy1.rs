abeff110aba70030cf1a8fef955ab850d2ba6498
freddydc <freddydiaz.liv@gmail.com>
2022-04-20 10:22:44 -0500

    fix(clippy1): Add approx_constant exercise

--- clippy/clippy1.rs
+++ clippy/clippy1.rs
@@ -6,6 +6,8 @@
 // check clippy's suggestions from the output to solve the exercise.
 // Execute `rustlings hint clippy1` for hints :)
 
+use std::f32;
+
 fn main() {
     let x = 1.2331_f64;
     let y = 1.2332_f64;
@@ -18,4 +20,14 @@ fn main() {
     if (x - 1.2331_f64).abs() < error_margin {
         println!("Pass!");
     }
+
+    let pi = 3.14f32;
+    let radius = 5.00f32;
+
+    let area = pi * f32::powi(radius, 2);
+
+    println!(
+        "The area of a circle with radius {:.2} is {:.5}!",
+        radius, area
+    )
 }
